a25f018858515f6741cfbc280901a8e73088f043
nullp2ike <risko.ruus@gmail.com>
2013-10-07 23:36:05 +0300

    Created views and controllers for rentit

---
+++ src/main/java/cs/ut/domain/PlantController.java
@@ -0,0 +1,10 @@
+package cs.ut.domain;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/plants")
+@Controller
+@RooWebScaffold(path = "plants", formBackingObject = Plant.class)
+public class PlantController {
+}
