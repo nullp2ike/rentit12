fe031a3783d16634dd21b5b0057f76d9481925c0
nullp2ike <risko.ruus@gmail.com>
2013-11-20 00:42:40 +0200

    Have to fix a bug of either storing or parsing the url in buildit

--- src/main/java/cs/ut/web/PurchaseOrderInvoiceController.java
+++ src/main/java/cs/ut/web/PurchaseOrderInvoiceController.java
@@ -1,37 +1,59 @@
 package cs.ut.web;
 
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.math.BigDecimal;
 import java.util.List;
 
+import javax.activation.DataSource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.util.ByteArrayDataSource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.mail.MailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import cs.ut.domain.HireRequestStatus;
 import cs.ut.domain.PurchaseOrder;
 import cs.ut.domain.bean.PurchaseOrderListDTO;
 import cs.ut.repository.PurchaseOrderRepository;
-import cs.ut.util.MailMail;
 
 @RequestMapping("/purchaseorders/approved/**")
 @Controller
 public class PurchaseOrderInvoiceController {
-	
-	@Autowired
-	private transient MailSender mailTemplate;
 
+	@Autowired
+	JavaMailSenderImpl sender;
+	
+	@Value("${webappurl}")
+	String webappurl;
+	
+	@Value("${email.username}")
+	String fromEmail;
+	
+	@Value("${email.buildit}")
+	String toEmail;
+	
 	@Autowired
 	PurchaseOrderRepository repository;
 
@@ -40,7 +62,7 @@ public class PurchaseOrderInvoiceController {
 			ModelMap modelMap) {
 		PurchaseOrderListDTO po = new PurchaseOrderListDTO();
 		List<PurchaseOrder> orderList = repository
-				.findForSendingInvoice(HireRequestStatus.CLOSED);
+				.findForSendingInvoice(HireRequestStatus.OPEN);
 		po.setOrderList(orderList);
 		modelMap.put("purchaseOrders", po);
 		return "purchaseorders/approved/list";
@@ -52,7 +74,12 @@ public class PurchaseOrderInvoiceController {
 		String selectedPurchaseOrder = request.getParameter("radio");
 		PurchaseOrder po = PurchaseOrder.findPurchaseOrder(Long
 				.parseLong(selectedPurchaseOrder));
-		sendInvoice();
+		try {
+			sendInvoice(createInvoice(po.getId().toString(), po.getTotalCost()));
+		} catch (ParserConfigurationException | TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		po.setInvoiceSent(true);
 		List<PurchaseOrder> orderList = repository
 				.findForSendingInvoice(HireRequestStatus.CLOSED);
@@ -61,13 +88,58 @@ public class PurchaseOrderInvoiceController {
 		return "purchaseorders/approved/list";
 	}
 
-	private void sendInvoice() {
-		
+	private void sendInvoice(String invoiceContent) {
+		MimeMessage message = sender.createMimeMessage();
 		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-				simpleMailMessage.setFrom("rentitteam12@gmail.com");
-				simpleMailMessage.setSubject("Testing123");
-				simpleMailMessage.setTo("builditteam12@gmail.com");
-				simpleMailMessage.setText("Testing only \n\n Hello Spring Email Sender");
-				mailTemplate.send(simpleMailMessage);
+		simpleMailMessage.setFrom(fromEmail);
+		simpleMailMessage.setSubject("Your invoice for purcase order");
+		simpleMailMessage.setTo(toEmail);
+		simpleMailMessage.setText("Hello, your invoice is attached to the email");
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setFrom(simpleMailMessage.getFrom());
+			helper.setTo(simpleMailMessage.getTo());
+			helper.setSubject(simpleMailMessage.getSubject());
+			helper.setText(simpleMailMessage.getText());
+			DataSource ds = null;
+			try {
+				ds = new ByteArrayDataSource(invoiceContent, "application/xml");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			helper.addAttachment("invoice.xml", ds);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+				sender.send(message);
+	}
+ 
+	private String createInvoice(String poId, BigDecimal totalSum) throws ParserConfigurationException, TransformerException{
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		
+		Document doc = docBuilder.newDocument();
+		doc.setXmlStandalone(false);
+		Element rootElement = doc.createElement("invoice");
+		doc.appendChild(rootElement);
+ 
+		
+		Element link = doc.createElement("purchaseOrderHRef");
+		link.setTextContent(webappurl + "/rest/pos/" + poId);
+		rootElement.appendChild(link);
+		
+		Element total = doc.createElement("total");
+		total.setTextContent(totalSum.toString());
+		rootElement.appendChild(total);
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer = tf.newTransformer();
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		String output = writer.getBuffer().toString();
+		return output;
 	}
 }
