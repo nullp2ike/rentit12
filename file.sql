5f0334f8375daaf19601326e8ab3a323be37c3cb
nullp2ike <risko.ruus@gmail.com>
2013-12-04 00:18:11 +0200

    Security HW done

--- file.sql
+++ file.sql
@@ -1,3 +1,8 @@
-insert into site_engineer VALUES (0, 'FirstName', 'LastName', 0);
-insert into site VALUES (0, 'SiteName', 0);
-insert into supplier VALUES (0, 'SupplierName', 0);
+insert into users VALUES (0, 't', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'user', 0);
+insert into users VALUES (1, 't', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'admin', 0);
+
+insert into authorities VALUES (0, 'ROLE_USER', 0);
+insert into authorities VALUES (1, 'ROLE_ADMIN', 0);
+
+insert into assignments VALUES(0, 0, 0, 0);
+insert into assignments VALUES(1, 0, 1, 1);
